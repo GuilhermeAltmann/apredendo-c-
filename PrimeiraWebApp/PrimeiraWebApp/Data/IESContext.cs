e706d8a1a340b56df3b1bf840fad8b5393d93110
Guilherme Altmann <altmannguilherme@gmail.com>
2018-03-24 11:38:39 -0300

    Commit

---
+++ PrimeiraWebApp/PrimeiraWebApp/Data/IESContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrimeiraWebApp.Models;
+
+namespace PrimeiraWebApp.Data
+{
+    public class IESContext : DbContext
+    {
+
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options) {
+
+            
+        }
+
+     
+    }
+}
--- PrimeiraWebApp/PrimeiraWebApp/Program.cs
+++ PrimeiraWebApp/PrimeiraWebApp/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PrimeiraWebApp.Data;
 
 namespace PrimeiraWebApp
 {
@@ -14,7 +16,29 @@ namespace PrimeiraWebApp
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+
+                var services = scope.ServiceProvider;
+
+                try
+                {
+
+                    var context = services.GetRequiredService<IESContext>();
+                    IESDbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro ocorreu ao popular a base de dados.");
+                }
+            }
+                host.Run();
+               // BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
